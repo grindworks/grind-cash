430185046a869a8695ce60a111145526c3493d10
koji.udagawa <koji@udagawa.biz>
2026-06-28 21:31:11 +0900

    Update assets and service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 💡 アップデート時はここを v2, v3... と書き換えることで更新が発火します
-const CACHE_NAME = 'grindcash-v20260628-8';
+const CACHE_NAME = 'grindcash-v20260628-9';
 const urlsToCache = [
   './',
   './index.html',
